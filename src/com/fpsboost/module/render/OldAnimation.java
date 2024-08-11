d103a36318d3d3e7f7be703ba396a33c0b49a35a
LangYa466 <langya466@gmail.com>
2024-08-11 16:52:20 +0800

    fix

--- src/com/fpsboost/module/render/OldAnimation.java
+++ src/com/fpsboost/module/render/OldAnimation.java
@@ -6,9 +6,9 @@ import com.fpsboost.value.impl.BooleanValue;
 
 @Module(value = "旧版本动画",category = Category.GUI)
 public class OldAnimation {
-    public static BooleanValue oldRod = new BooleanValue("鱼竿动画", false);
-    public static BooleanValue oldBlock = new BooleanValue("放置动画", false);
-    public static BooleanValue blockHit = new BooleanValue("防砍动画", false);
-    public static BooleanValue oldBow = new BooleanValue("弓箭动画", false);
-    public static BooleanValue oldSwing = new BooleanValue("挥手动画", false);
+    public static BooleanValue oldRod = new BooleanValue("鱼竿动画", true);
+    public static BooleanValue oldBlock = new BooleanValue("放置动画", true);
+    public static BooleanValue blockHit = new BooleanValue("防砍动画", true);
+    public static BooleanValue oldBow = new BooleanValue("弓箭动画", true);
+    public static BooleanValue oldSwing = new BooleanValue("挥手动画", true);
 }
