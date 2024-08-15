ceea22f9e2d1756c2cf8cbd0fde3e275cb305405
LangYa466 <langya466@gmail.com>
2024-08-15 15:10:57 +0800

    fix

---
+++ src/com/fpsboost/module/boost/NoClickDelay.java
@@ -0,0 +1,22 @@
+package com.fpsboost.module.boost;
+
+import com.fpsboost.Access;
+import com.fpsboost.annotations.event.EventTarget;
+import com.fpsboost.annotations.system.Module;
+import com.fpsboost.annotations.system.Startup;
+import com.fpsboost.events.update.UpdateEvent;
+import com.fpsboost.module.Category;
+
+@Startup
+@Module(value = "移除特殊点击延迟", category = Category.Boost)
+public class NoClickDelay implements Access.InstanceAccess {
+
+    @EventTarget
+    public void onUpdate(UpdateEvent event) {
+        if (mc.theWorld != null && mc.thePlayer != null) {
+            if (!mc.inGameHasFocus) return;
+
+            mc.leftClickCounter = 0;
+        }
+    }
+}
