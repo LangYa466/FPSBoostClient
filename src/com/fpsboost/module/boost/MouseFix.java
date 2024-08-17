2793d2412e4354ff24b5ecc4a2ba557b5c51042a
LangYa466 <langya466@gmail.com>
2024-08-17 23:40:51 +0800

    fix

--- src/com/fpsboost/module/boost/MouseFix.java
+++ src/com/fpsboost/module/boost/MouseFix.java
@@ -6,7 +6,7 @@ import com.fpsboost.annotations.system.Module;
 import com.fpsboost.module.Category;
 import com.fpsboost.util.mousefix.RawInputMod;
 
-@Module(name = "MouseFix",description = "鼠标移动更丝滑",category = Category.Boost)
+@Module(name = "MouseFix",description = "鼠标移动更丝滑 手机不能开",category = Category.Boost)
 public class MouseFix {
     private final RawInputMod rawInputMod = new RawInputMod();
 
